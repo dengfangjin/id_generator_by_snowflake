ec4b220aa9555f45e27acf5fdd147c2d1d222031
dengfangjin <45445350@qq.com>
2020-07-14 11:14:13 +0800

    master

--- start_snowflake_server/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""
-Project: id_generator
-Author : Advance
-Time   : 2020-07-09 15:04
-Desc   :
-"""
--- start_snowflake_server/start.sh
+++
@@ -1,1 +0,0 @@
-nohup python start_snowflake.py >snowflake.log &
